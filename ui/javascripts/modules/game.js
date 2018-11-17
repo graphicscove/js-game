2b1807f588168ff048ad9b66f0a6b262defdcd0c
Steven Noble <steven@graphicscove.com>
2018-11-17 17:16:38 +0000

    Option to now train troops

--- ui/javascripts/modules/game.js
+++ ui/javascripts/modules/game.js
@@ -79,17 +79,34 @@ class Game {
                     },
                     costMultiplier: 2.5
                 }
+            },
+            troops: {
+                scout: {
+                    name: 'Scout',
+                    cost: {
+                        food: 50,
+                        wood: 0,
+                        stone: 0,
+                        ore: 0,
+                        gold: 0
+                    },
+                    count: 0,
+                    time: 2
+                }
             }
         }
 
         this.cityInfo()
         this.resourceInfo()
         this.buildingsInfo()
+        this.troopsInfo()
         this.marketplaceInfo()
 
         $('body').on('click', '[data-behaviour="upgrade"]', this.buildingsUpgrade)
+        $('body').on('click', '[data-behaviour="train"]', this.trainTroops)
         $('[data-behaviour="save-game"]').on('click', this.saveGame)
         $('[data-behaviour="load-save"]').on('click', this.loadGame)
+
         $('[data-behaviour="purchase"]').on('click', this.purchaseResources)
 
         window.setInterval(this.resourceProduction, 1000)
@@ -159,6 +176,36 @@ class Game {
         $('[data-element="buildings"]').html(this.buildingTemplate)
     }
 
+    // HTML output to show troops
+    troopsInfo = (e) => {
+        const self = this
+        this.troopsTemplate = ''
+
+        // Show toops, purchase requirements and purchase button
+        Object.keys(this.city.troops).map(function(objectKey, index) {
+            const name = self.city.troops[objectKey].name
+            self.resourceRequirement = ''
+            self.resourceRequirement += `Food: ${self.city.troops[objectKey].cost.food} `
+            self.resourceRequirement += `Wood: ${self.city.troops[objectKey].cost.wood} `
+            self.resourceRequirement += `Stone: ${self.city.troops[objectKey].cost.stone} `
+            self.resourceRequirement += `Ore: ${self.city.troops[objectKey].cost.ore} `
+            self.resourceRequirement += `Gold: ${self.city.troops[objectKey].cost.gold} `
+
+            const troopsTemplate = `
+            <div class="grid grid--space-between">
+                <div>
+                    <p><strong>${name}</strong> - ${self.city.troops[objectKey].count} in barracks (${self.city.troops[objectKey].time}s)<p>
+                    <p>Training Cost: ${self.resourceRequirement}</p>
+                </div>
+                <button data-behaviour="train" data-type="${objectKey}" class="button button--primary">
+                    Train
+                </button>
+            </div>`
+            self.troopsTemplate += troopsTemplate
+        });
+        $('[data-element="troops"]').html(this.troopsTemplate)
+    }
+
     // Resource multiplier times building level increases resources
     resourceProduction = (e) => {
         const self = this
@@ -226,13 +273,63 @@ class Game {
         this.resourceInfo()
     }
 
+    // Train troops
+    trainTroops = (e) => {
+        const upgradeType = $(e.currentTarget).data('type')
+        const newLevel = this.city.troops[upgradeType].count + 1
+
+        // Check to see if there are enough resources
+        if (this.city.resources.food < this.city.troops[upgradeType].cost.food ||
+            this.city.resources.wood < this.city.troops[upgradeType].cost.wood ||
+            this.city.resources.stone < this.city.troops[upgradeType].cost.stone ||
+            this.city.resources.ore < this.city.troops[upgradeType].cost.ore ||
+            this.city.resources.gold < this.city.troops[upgradeType].cost.gold ) {
+            Notification.openNotification('error', 'Not enough resources')
+            return false;
+        }
+
+        Notification.openNotification('success', `Purchase of 1 ${upgradeType} in progress`)
+
+        const self = this
+
+        // Disable clicking any upgrade button again before the upgrade is complete
+        $('[data-behaviour="train"]').attr('disabled', true)
+
+        // Remove resources from pool to intiate upgrade
+        this.city.resources.food -= this.city.troops[upgradeType].cost.food
+        this.city.resources.wood -= this.city.troops[upgradeType].cost.wood
+        this.city.resources.stone -= this.city.troops[upgradeType].cost.stone
+        this.city.resources.ore -= this.city.troops[upgradeType].cost.ore
+        this.city.resources.gold -= this.city.troops[upgradeType].cost.gold
+
+        var tm = setInterval(countDown, 1000);
+        let timer = this.city.troops[upgradeType].time
+        function countDown(){
+           timer -= self.city.troops[upgradeType].time
+           if( timer === 0 ){
+              clearInterval(tm)
+              addPurchase()
+           }
+        }
+
+        function addPurchase() {
+            const currentCount = self.city.troops[upgradeType].count
+            self.city.troops[upgradeType].count = currentCount + 1
+            self.troopsInfo()
+            Notification.closeNotification()
+        }
+
+        // Update new resource totals
+        this.resourceInfo()
+    }
+
     marketplaceInfo = (e) => {
         this.marketplaceTemplate = `<p>All purchases cost 100 gold</p>
         <p>
-        <button data-behaviour="purchase" data-resource-type="food">Buy 100 Food</button>
-        <button data-behaviour="purchase" data-resource-type="wood">Buy 100 Wood</button>
-        <button data-behaviour="purchase" data-resource-type="stone">Buy 100 Stone</button>
-        <button data-behaviour="purchase" data-resource-type="ore">Buy 100 Ore</button>
+        <button class="button button--primary" data-behaviour="purchase" data-resource-type="food">Buy 100 Food</button>
+        <button class="button button--primary" data-behaviour="purchase" data-resource-type="wood">Buy 100 Wood</button>
+        <button class="button button--primary" data-behaviour="purchase" data-resource-type="stone">Buy 100 Stone</button>
+        <button class="button button--primary" data-behaviour="purchase" data-resource-type="ore">Buy 100 Ore</button>
         </p>`
         $('[data-element="marketplace"]').html(this.marketplaceTemplate)
     }
